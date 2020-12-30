6f97e1ac4f3f0b660a3eb6766ca9d63b9ad6f0e9
Borek Pozar <pozar.borek@gmail.com>
2020-12-30 08:40:19 +0000

    Created files for first classes.

--- training-app/src/main/java/model/user.java
+++ training-app/src/main/java/model/user.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package team.training.app;
+package model;
 
 /**
  *
  * @author borek
  */
-public class model {
+public class user {
     
 }
